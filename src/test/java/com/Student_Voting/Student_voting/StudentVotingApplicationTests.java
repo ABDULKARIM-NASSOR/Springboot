39aae0a891d6dfede0333a9396b663150ec60d0b
unknown <karimabdul.anzuan.07@gmail.com>
2024-07-19 02:44:24 -0900

    first commit

---
+++ src/test/java/com/Student_Voting/Student_voting/StudentVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Student_Voting.Student_voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
